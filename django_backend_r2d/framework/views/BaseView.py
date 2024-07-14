6a42287f3e6902fd99a82fe7e4139e29f967c1d0
yapping72 <yapping72@gmail.com>
2024-07-14 13:05:01 +0800

    Update exceptions caught in BaseView

--- django_backend_r2d/framework/views/BaseView.py
+++ django_backend_r2d/framework/views/BaseView.py
@@ -9,6 +9,8 @@ from authentication.services.AuthenticationExceptions import *
 from jobs.services.JobExceptions import *
 from model_manager.services.ModelExceptions import *
 from diagrams.services.DiagramExceptions import *
+from diagrams.services.DiagramConsumerExceptions import *
+from notifications.services.NotificationExceptions import *
 from rest_framework.exceptions import ValidationError
 
 import logging
@@ -35,6 +37,7 @@ class BaseView(APIView):
                 # Log the error for auditing purposes
                 logger.error(f"Serialization Validation error: {str(e)}", exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message ="Validation error during serialization", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            # Accounts and Authentication Exceptions
             except UserAlreadyExistsError as e:
                 logger.error(repr(e), exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message ="Username or email already in use", success = False,  status_code=status.HTTP_400_BAD_REQUEST).transform()
@@ -68,12 +71,20 @@ class BaseView(APIView):
             except CompromisedPasswordError as e:
                 logger.error(repr(e), exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message = "The password you have provided is not secure and has been found in databases of leaked passwords. For your safety, please choose a different password that has not been compromised.", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            # Notification Exceptions
+            except SendEmailNotificationError as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Accounts or notification service is down please contact administrators if the problem persists", success = False, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR).transform()
+            # Job Exceptions
             except JobCreationException as e:
                 logger.error(repr(e), exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message = "Failed to create Job Record", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
             except JobUpdateException as e:
                 logger.error(repr(e), exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message = "Failed to update Job Record", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            except JobDeleteException as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Failed to delete Job Record", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
             except JobNotFoundException as e:
                 logger.error(repr(e), exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message = "Job not found", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
@@ -83,18 +94,59 @@ class BaseView(APIView):
             except AddToJobQueueException as e:
                 logger.error(repr(e), exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message = "Failed to add Job to Job Queue", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            except UpdateJobQueueException as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Failed to update Job in Job Queue", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            except RemoveFromJobQueueException as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Failed to remove Job from Job Queue", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            except RetrieveJobHistoryException as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Failed to retrieve Job History", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
             except AuditorInitializationError as e:
                 logger.error(repr(e), exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message = "Failed to initialize Auditor", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
             except ModelInitializationError as e:
                 logger.error(repr(e), exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message = "Failed to initialize Model", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            # DiagramExceptions
             except UMLDiagramCreationError as e:
                 logger.error(repr(e), exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message = "Failed to create UML Diagram", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
             except ClassDiagramSavingError as e:
                 logger.error(repr(e), exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message = "Failed to save Class Diagram", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()            
+            except ClassDiagramRetrievalError as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Failed to retrieve Class Diagrams", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()  
+            except ERDiagramSavingError as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Failed to save ER Diagram", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            except ERDiagramRetrievalError as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Failed to retrieve ER Diagrams", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            # DiagramConsumerExceptions
+            except ClassDiagramTaskError as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Error occurred in Class Diagram Task", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            except ClassDiagramConsumerError as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Error occurred in Class Diagram Consumer", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            except ClassDiagramSignalError as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Error occurred in Class Diagram Signal", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            except ERDiagramTaskError as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Error occurred in ER Diagram Task", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            except ERDiagramConsumerError as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Error occurred in ER Diagram Consumer", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            except ERDiagramSignalError as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Error occurred in ER Diagram Signal", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
+            except DiagramCreationSignalError as e:
+                logger.error(repr(e), exc_info=True)
+                return JSONResponse(data = {"error": str(e)}, message = "Error occurred in Signal", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
             except ValidationError as e:
                 logger.error(repr(e), exc_info=True)
                 return JSONResponse(data = {"error": str(e)}, message = "Validation error occurred during Serialization", success = False, status_code=status.HTTP_400_BAD_REQUEST).transform()
